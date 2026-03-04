21d5742ca69c23f0000dc804eef0bded77150c73
Santiago Sabogal Lozano <ssabogall@eafit.edu.co>
2026-03-04 10:08:29 -0500

    refactor test_lab.py and add test on file

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -4,8 +4,20 @@
 # - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
 # - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
 
-from palindromo import es_palindromo
-from utils import suma
+from python_app.palindromo import es_palindromo
+from python_app.utils import suma
 
 def test_ejemplo_siembra():
     assert True
+
+def test_es_palindromo():
+    assert es_palindromo("radar") == True
+    assert es_palindromo("Radar") == True
+    assert es_palindromo("anita lava la tina") == True
+    assert es_palindromo("python") == False
+    assert es_palindromo("") == True
+
+def test_suma():
+    assert suma(2,3) == 5
+    assert suma(0,5) == 5
+    assert suma(-2,3) == 1
